555e9966feef1d6961b2cb498a16a91b9c9c5351
NakuRei <nakurei7901@gmail.com>
2024-12-15 16:00:15 +0900

    feat: add ESLint stylistic rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,43 +4,45 @@ import tseslint from 'typescript-eslint';
 import tsParser from "@typescript-eslint/parser";
 
 import myTypescriptConfig from './nakurei-typescript-config.mjs';
-
+import myStylisticConfig from './nakurei-stylistic-config.mjs';
 
 export default tseslint.config(
-    {
-        files: ["**/*.ts"],
+  {
+    files: ["**/*.ts"],
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
     },
-    {
-        plugins: {
-            "@typescript-eslint": typescriptEslint,
-        },
-        extends: [
-            js.configs.recommended,
-            ...tseslint.configs.strictTypeChecked,
-            ...tseslint.configs.stylisticTypeChecked,
-            ...myTypescriptConfig,
-        ],
-        languageOptions: {
-            parser: tsParser,
-            ecmaVersion: 2022,
-            sourceType: "module",
-            parserOptions: {
-                project: ['./tsconfig.json'],
-            },
-        },
-
-        rules: {
-            "@typescript-eslint/naming-convention": ["warn", {
-                selector: "import",
-                format: ["camelCase", "PascalCase"],
-            }],
-
-            curly: "warn",
-            eqeqeq: "warn",
-            "no-throw-literal": "warn",
-            semi: "warn",
-
-            'max-lines-per-function': ['error', { max: 50 }],
-        },
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      ...myTypescriptConfig,
+    ],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: "module",
+      parserOptions: {
+          project: ['./tsconfig.json'],
+      },
     },
+
+    rules: {
+      "@typescript-eslint/naming-convention": ["warn", {
+        selector: "import",
+        format: ["camelCase", "PascalCase"],
+      }],
+
+      curly: "warn",
+      eqeqeq: "warn",
+      "no-throw-literal": "warn",
+      semi: "warn",
+
+      'max-lines-per-function': ['error', { max: 50 }],
+    },
+  },
+  {
+    extends: [...myStylisticConfig],
+    files: ['**/*.{js,ts}'],
+  },
 );
