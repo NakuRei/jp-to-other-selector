9ad3ad887b826bec43fd9908de2947c06c9dba49
NakuRei <nakurei7901@gmail.com>
2024-12-22 14:26:05 +0900

    test: add tests for cursor movement commands

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -5,13 +5,61 @@ import * as assert from 'assert';
  * as well as import your extension to test it
  */
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+
+async function setupEditor(
+  text: string,
+  position: vscode.Position,
+): Promise<vscode.TextEditor> {
+  const editor = await vscode.window.showTextDocument(
+    vscode.Uri.parse('untitled:test'),
+  );
+  await editor.edit((editBuilder) => {
+    editBuilder.insert(new vscode.Position(0, 0), text);
+  });
+  editor.selection = new vscode.Selection(position, position);
+  return editor;
+}
 
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.');
 
-  test('Sample test', () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-  });
+  test('moveCursorRight command should be registered',
+    () => {
+      vscode.commands.getCommands(true).then((commands) => {
+        assert.ok(commands.includes('jp-to-other-selector.moveCursorRight'));
+      });
+    });
+
+  test('moveCursorLeft command should be registered',
+    () => {
+      vscode.commands.getCommands(true).then((commands) => {
+        assert.ok(commands.includes('jp-to-other-selector.moveCursorLeft'));
+      });
+    });
+
+  test('moveCursorRight should move the cursor to the right by one word',
+    async() => {
+      const editor = await setupEditor(
+        'test word',
+        new vscode.Position(0, 0),
+      );
+      await vscode.commands.executeCommand(
+        'jp-to-other-selector.moveCursorRight',
+      );
+      // 'test 'の後にカーソルが移動
+      assert.strictEqual(editor.selection.active.character, 4);
+    });
+
+  test('moveCursorLeft should move the cursor to the left by one word',
+    async() => {
+      const editor = await setupEditor(
+        'test word',
+        new vscode.Position(0, 9),
+      );
+      await vscode.commands.executeCommand(
+        'jp-to-other-selector.moveCursorLeft',
+      );
+      // 'word'の前にカーソルが移動
+      assert.strictEqual(editor.selection.active.character, 5);
+    });
 });
