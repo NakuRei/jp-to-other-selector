365bb8be60108c167b39dddca43091915cbf23d1
NakuRei <nakurei7901@gmail.com>
2024-12-23 00:35:14 +0900

    feat: support multi-cursor movement

--- src/functions/moveCursorToNextBoundary.ts
+++ src/functions/moveCursorToNextBoundary.ts
@@ -8,15 +8,30 @@ export async function moveCursorToNextBoundary(): Promise<void> {
     return;
   }
   const activeDocument = editor.document;
-  const currentPosition = editor.selection.active;
 
-  const newPosition = findNextBoundaryPosition(
-    activeDocument, currentPosition,
-  );
-  if (newPosition) {
-    editor.selection = new vscode.Selection(newPosition, newPosition);
-  } else {
-    // 通常の「次の単語」へ移動
+  // 新しいカーソル位置を計算
+  const newSelections = editor.selections.map((selection) => {
+    const currentPosition = selection.active;
+    const newPosition = findNextBoundaryPosition(
+      activeDocument, currentPosition,
+    );
+    if (newPosition) {
+      return new vscode.Selection(newPosition, newPosition);
+    }
+    // 境界が見つからない場合
+    return null;
+  });
+
+  // 境界が見つからなかったカーソルが存在する場合の処理
+  if (newSelections.some((selection) => selection === null)) {
+    // VSCodeのコマンドで「次の単語」に移動させる
     await vscode.commands.executeCommand('cursorWordEndRight');
+
+    // コマンド実行後のカーソル位置をすべて取得
+    const updatedSelections = vscode.window.activeTextEditor?.selections ?? [];
+    editor.selections = updatedSelections;
+  } else {
+    // 新しいカーソル位置を適用
+    editor.selections = newSelections as vscode.Selection[];
   }
 }
